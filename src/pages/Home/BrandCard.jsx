82a856dbfbcfca7d622e3e612a3638efab0fff30
AdnanSakib04 <navidadnan04@gmail.com>
2023-10-20 13:28:10 +0600

    brands component updated

--- src/pages/Home/BrandCard.jsx
+++ src/pages/Home/BrandCard.jsx
@@ -3,12 +3,17 @@ const BrandCard = ({ singleBrand }) => {
     const { photo, brand } = singleBrand;
     return (
         <div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src={photo} className="max-h-[240px] p-2" alt="Brands" /></figure>
+            <div className="card card-compact w-80 bg-base-100 shadow-xl h-[320px]">
+               <div className=" ">
+                <div className="">
+                <figure ><img src={photo} className="h-[240px] p-2" alt="Brands" /></figure>
+
+                </div>
                 <div className="card-body">
                     <h2 className="text-center text-3xl font-bold">{brand}</h2>
                     
                 </div>
+               </div>
             </div>
 
         </div>
--- src/pages/Home/Brands.jsx
+++ src/pages/Home/Brands.jsx
@@ -8,7 +8,8 @@ const Brands = () => {
     return (
         <div className='m-20'>
         <h1 className='text-6xl text-center my-20 text-purple-600'>Our Brands {brands.length}</h1>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        <div className="max-w-7xl mx-auto">
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-6 md:p-0  gap-y-6 mb-20 justify-items-center'>
           {
             brands.map(singleBrand => <BrandCard
                 key={singleBrand._id}
@@ -17,6 +18,8 @@ const Brands = () => {
             </BrandCard>)
           }
         </div>
+        </div>
+       
       </div>
     );
 };
