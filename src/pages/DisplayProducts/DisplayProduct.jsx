af53c52ffd89f730c670b552fb15d2c2b828bf99
AdnanSakib04 <navidadnan04@gmail.com>
2023-10-21 12:00:37 +0600

    display products component updated

--- src/pages/DisplayProducts/DisplayProduct.jsx
+++ src/pages/DisplayProducts/DisplayProduct.jsx
@@ -10,9 +10,12 @@ const DisplayProducts = () => {
        
          <div className="max-w-7xl mx-auto">
          <Slider></Slider>
+         {products.length === 0 && (
+                    <h1 className="text-4xl font-bold text-center mt-40  text-red-500 mx-auto">No Products Available </h1>
+                ) }
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-6 md:p-0  gap-y-6 mb-20 justify-items-center'>
             {products.length === 0 ? (
-                    <h1 className="text-3xl font-bold text-center my-40 text-red-700 mx-auto">No products available</h1>
+                    <h1 className="text-3xl font-bold text-center my-40 text-red-700 mx-auto"></h1>
                 ) : (
                     products.map(product => (
                         <ProductCard key={product._id}
